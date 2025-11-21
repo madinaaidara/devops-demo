c8e3121556e74dbc4be9d3c79205feb03c43934b
Madina Aidara <madinaaidara26@gmail.com>
2025-11-21 12:39:52 +0100

    Ajout CI GitHub Actions 1

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
-
-Route::get('/', function(){ return redirect()->route('products.index'); });
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/products', function(){ return redirect()->route('products.index'); });
 Route::resource('products', ProductController::class);
